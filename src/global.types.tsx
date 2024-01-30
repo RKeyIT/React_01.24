f1389c7208bc8e021baeafe01775f578d65756e4
Raman Krytski <rkeydevelop@gmail.com>
2024-01-30 17:41:08 +0100

    types: changed global types

--- src/global.types.tsx
+++ src/global.types.tsx
@@ -1,39 +1,57 @@
-/* 
-    2. Select input for “category” with required values 
-    3. Select input for “difficulty” with required values (any || easy || medium || hard)
-    4. Select input for “type” with required values (any || multiple choice || true/false)
-    5. Select input for “time” with values: 1m, 2m, 5m
-     */
+export interface ISelectOption {
+  id: string;
+  name: string;
+}
 
-export const QuizCategories = Object.freeze({
-  ANY: { id: 999999, name: 'any' },
-  GENERAL: { id: 9, name: 'General Knowledge' },
-  EntBooks: { id: 10, name: 'Entertainment: Books' },
-  EntFilms: { id: 11, name: 'Entertainment: Film' },
-  EntMusic: { id: 12, name: 'Entertainment: Music' },
-  EntMusicalsTheatres: { id: 13, name: 'Entertainment: Musicals & Theatres' },
-  EntTelevision: { id: 14, name: 'Entertainment: Television' },
-  EntVideoGames: { id: 15, name: 'Entertainment: Video Games' },
-  EntBoardGames: { id: 16, name: 'Entertainment: Board Games' },
-  ScienceNature: { id: 17, name: 'Science & Nature' },
-  ScienceCoputers: { id: 18, name: 'Science: Computers' },
-  ScienceMathematics: { id: 19, name: 'Science: Mathematics' },
-  Mythology: { id: 20, name: 'Mythology' },
-  Sports: { id: 21, name: 'Sports' },
-  Geography: { id: 22, name: 'Geography' },
-  History: { id: 23, name: 'History' },
-  Politics: { id: 24, name: 'Politics' },
-  Art: { id: 25, name: 'Art' },
-  Celebrities: { id: 26, name: 'Celebrities' },
-  Animals: { id: 27, name: 'Animals' },
-  Vehicles: { id: 28, name: 'Vehicles' },
-  EntComics: { id: 29, name: 'Entertainment: Comics' },
-  ScienceGadgets: { id: 30, name: 'Science: Gadgets' },
+interface ICategoriesObject {
+  [key: string]: ISelectOption;
+}
+
+export const QuizCategories: ICategoriesObject = Object.freeze({
+  ANY: { id: 'any', name: 'Any Category' },
+  GENERAL: { id: '9', name: 'General Knowledge' },
+  EntBooks: { id: '10', name: 'Entertainment: Books' },
+  EntFilms: { id: '11', name: 'Entertainment: Film' },
+  EntMusic: { id: '12', name: 'Entertainment: Music' },
+  EntMusicalsTheatres: { id: '13', name: 'Entertainment: Musicals & Theatres' },
+  EntTelevision: { id: '14', name: 'Entertainment: Television' },
+  EntVideoGames: { id: '15', name: 'Entertainment: Video Games' },
+  EntBoardGames: { id: '16', name: 'Entertainment: Board Games' },
+  ScienceNature: { id: '17', name: 'Science & Nature' },
+  ScienceCoputers: { id: '18', name: 'Science: Computers' },
+  ScienceMathematics: { id: '19', name: 'Science: Mathematics' },
+  Mythology: { id: '20', name: 'Mythology' },
+  Sports: { id: '21', name: 'Sports' },
+  Geography: { id: '22', name: 'Geography' },
+  History: { id: '23', name: 'History' },
+  Politics: { id: '24', name: 'Politics' },
+  Art: { id: '25', name: 'Art' },
+  Celebrities: { id: '26', name: 'Celebrities' },
+  Animals: { id: '27', name: 'Animals' },
+  Vehicles: { id: '28', name: 'Vehicles' },
+  EntComics: { id: '29', name: 'Entertainment: Comics' },
+  ScienceGadgets: { id: '30', name: 'Science: Gadgets' },
   EntJapaneseAnimeManga: {
-    id: 31,
+    id: '31',
     name: 'Entertainment: Japanese Anime & Manga',
   },
-  EntCartoonAnimations: { id: 32, name: 'Entertainment: Cartoon & Animations' },
+  EntCartoonAnimations: {
+    id: '32',
+    name: 'Entertainment: Cartoon & Animations',
+  },
+});
+
+export const QuizDifficulties = Object.freeze({
+  ANY: { id: 'any', name: 'Any Difficulty' },
+  easy: { id: 'easy', name: 'Easy' },
+  medium: { id: 'medium', name: 'Medium' },
+  hard: { id: 'hard', name: 'Hard' },
+});
+
+export const QuizType = Object.freeze({
+  ANY: { id: 'any', name: 'Any Type' },
+  MULTIPLE: { id: 'multiple', name: 'Multiple Choice' },
+  BOOLEAN: { id: 'boolean', name: 'True / False' },
 });
 
 // export enum QuizCategoriesIDs {
@@ -63,17 +81,17 @@ export const QuizCategories = Object.freeze({
 //   EntertCartoonAnimations,
 // }
 
-export enum QuizDifficulty {
+export enum QuizDifficultyEnum {
   ANY = 'any',
   EASY = 'easy',
   MEDIUM = 'medium',
   HARD = 'hard',
 }
 
-export enum QuizType {
+export enum QuizTypeEnum {
   ANY = 'any',
   MULTIPLE = 'multiple',
   BOOLEAN = 'boolean',
 }
 
-export type QuizTimeForAnswer = 1 | 2 | 5;
+export type QuizTimeForAnswerType = 1 | 2 | 5;
