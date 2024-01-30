61400091b0f90213e729b994af9571da6b0f2bc5
Raman Krytski <rkeydevelop@gmail.com>
2024-01-30 15:03:15 +0100

    init: task written

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ export const App = () => {
   return (
     <div className={styles.App}>
       <div className={styles.container}>
+        <Home />
       </div>
     </div>
   );
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import styles from './Home.module.css';
+
+/* 
+    1. Number input for “number of questions”. Limit from 5 to 15
+    2. Select input for “category” with required values
+    3. Select input for “difficulty” with required values
+    4. Select input for “type” with required values
+    5. Select input for “time” with values: 1m, 2m, 5m
+    6. Button “Start quiz”
+    7. Button “See my statistics”
+*/
+
+export const Home = () => {
+  return <div className={styles.Home}></div>;
+};
