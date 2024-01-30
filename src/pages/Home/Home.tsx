5152b7d42510d74c81d5a65f18e9b50f8ab0e993
Raman Krytski <rkeydevelop@gmail.com>
2024-01-30 16:01:38 +0100

    task: 1 task with number inpud closed

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,8 @@
+import { NumberInput } from '../../entities/NumberInput/NumberInput';
 import styles from './Home.module.css';
 
 /* 
-    1. Number input for “number of questions”. Limit from 5 to 15
+    1. - OK - Number input for “number of questions”. Limit from 5 to 15
     2. Select input for “category” with required values
     3. Select input for “difficulty” with required values
     4. Select input for “type” with required values
@@ -11,5 +12,9 @@ import styles from './Home.module.css';
 */
 
 export const Home = () => {
-  return <div className={styles.Home}></div>;
+  return (
+    <div className={styles.Home}>
+      <NumberInput min={5} max={15} />
+    </div>
+  );
 };
