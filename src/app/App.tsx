b49bc665eb4edd0eba57e152fccb7dd798aafd2e
Roman Krytski <rkeydevelop@gmail.com>
2024-01-31 11:05:15 +0100

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,7 +4,7 @@ import { Home } from '../pages/Home/Home'
 
 export const App = () => {
   return (
-    <div className={`${styles.App}`}>
+    <div className={styles.App}>
       <h1 className={styles.Header}>QUIZzTRON</h1>
       <div className={styles.container}>
         <Home />
