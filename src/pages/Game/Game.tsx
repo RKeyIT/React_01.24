0d4bf693a89afeb73a1b68ae736341e4bd27562c
Raman Krytski <rkeydevelop@gmail.com>
2024-02-01 12:17:57 +0100

    feat: base pages structure

---
+++ src/pages/Game/Game.tsx
@@ -0,0 +1,9 @@
+import styles from './Game.module.css'
+import { FC } from 'react'
+
+export const Game: FC = () => {
+  return (
+    <div className={styles.Game}>
+    </div>
+  );
+};
---
+++ src/pages/Result/Result.tsx
@@ -0,0 +1,9 @@
+import styles from './Result.module.css'
+import { FC } from 'react'
+
+export const Result: FC = () => {
+  return (
+    <div className={styles.Result}>
+    </div>
+  );
+};
