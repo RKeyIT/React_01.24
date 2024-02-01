dedf597647f8322232e36c13742022c80efc8f34
Raman Krytski <rkeydevelop@gmail.com>
2024-02-01 21:48:12 +0100

    feat: local state was replaced by outer reducer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,13 @@
 import styles from './App.module.css'
 import { Home } from '../pages/Home/Home'
+import { Heading } from '../shared/Heading/Heading'
 
 export const App = () => {
   return (
     <div className={styles.App}>
       <h1 className={styles.Header}>QUIZzTRON</h1>
           <div className={styles.container}>
-            <Heading pageName={context.currentPage}/>
+            <Heading pageName='home'/>
             <Home />
           </div>
       <h2 className={styles.Footer}>QUIZzTRON</h2>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,76 +4,41 @@ import styles from './Home.module.css'
 import { Select } from '../../shared/Select/Select'
 import { QuizCategories, QuizDifficulties, QuizTime, QuizType } from '../../global.types'
 import { useEffect, useState } from 'react'
+import { ActionTypeEnum, useHomeReducer } from '../../reducers/HomeReducer'
 
 export const Home = () => {
-  // FIXME - Create reducer to replece logic of this component
-  const BASEURL = `https://opentdb.com/api.php?`
+  // TODO - Rebuild the component using Context
+  // Request example: https://opentdb.com/api.php?amount=10&category=22&difficulty=medium&type=multiple
+  const BASE_API_URL = `https://opentdb.com/api.php?`
+  const MIN_QUESTION_AMOUNT = 5;
+  const MAX_QUESTION_AMOUNT = 15
 
-  const [state, setState] = useState({
-    questionAmountMin: 5,
-    questionAmountMax: 15,
-    currentQuestionAmount: 5,
-    category: QuizCategories.ANY.id,
-    difficulty: QuizDifficulties.ANY.id,
-    type: QuizType.ANY.id,
-    time: QuizTime.ANY.id
-  })
+  const [state, dispatch] = useHomeReducer()
 
   const [apiURL, setApiURL] = useState(
-    `https://opentdb.com/api.php?amount=${state.currentQuestionAmount}`
+    `${BASE_API_URL}amount=${state.questionAmount}`
   )
 
-  const setCategory = (id: string) => {
-    setState((prev) => ({
-      ...prev,
-      category: id
-    }))
-  }
-  const setDifficulty = (id: string) => {
-    setState((prev) => ({
-      ...prev,
-      difficulty: id
-    }))
-  }
-  const setType = (id: string) => {
-    setState((prev) => ({
-      ...prev,
-      type: id
-    }))
-  }
-  const setTime = (id: string) => {
-    setState((prev) => ({
-      ...prev,
-      time: id
-    }))
-  }
-  const setQuestionAmount = (count: number) => {
-    setState((prev) => ({
-      ...prev,
-      currentQuestionAmount: count
-    }))
-  }
+  const setQuestionAmount = (id: number) => dispatch({type: ActionTypeEnum.AMOUNT, payload: String(id)})
+  const setCategory = (id: string) => dispatch({type: ActionTypeEnum.CATEGORY, payload: id})
+  const setDifficulty = (id: string) => dispatch({type: ActionTypeEnum.DIFFICULTY, payload: id})
+  const setType = (id: string) =>  dispatch({type: ActionTypeEnum.TYPE, payload: id})
+  const setTime = (id: string) => dispatch({type: ActionTypeEnum.TIME, payload: id})
 
-  const onStartHandler = () => {
-    console.log(apiURL)
-  }
-
-  const onShowStatisticsHandler = () => {
-    console.log('STATISTICS')
-  }
+  const onStartHandler = () => console.log(state.time, apiURL)
+  const onShowStatisticsHandler = () => console.log('STATISTICS')
 
   useEffect(() => {
-    const { category, currentQuestionAmount, difficulty, type } = state
-    // Request example: https://opentdb.com/api.php?amount=10&category=22&difficulty=medium&type=multiple
+    const { category, questionAmount, difficulty, type } = state
 
-    let urlParams = 'amount=' + currentQuestionAmount
+    let urlParams = 'amount=' + questionAmount
 
     if (category !== 'any') urlParams += '&category=' + category
     if (difficulty !== 'any') urlParams += '&difficulty=' + difficulty
     if (type !== 'any') urlParams += '&type=' + type
 
-    setApiURL(BASEURL + urlParams)
-  }, [state, BASEURL])
+    setApiURL(BASE_API_URL + urlParams)
+  }, [state, BASE_API_URL])
 
   return (
     <div className={styles.Home}>
@@ -86,8 +51,8 @@ export const Home = () => {
       <div className={styles.input}>
         <NumberInput
           callback={setQuestionAmount}
-          min={state.questionAmountMin}
-          max={state.questionAmountMax}
+          min={MIN_QUESTION_AMOUNT}
+          max={MAX_QUESTION_AMOUNT}
           label="Chose the number of questions: "
         />
       </div>
