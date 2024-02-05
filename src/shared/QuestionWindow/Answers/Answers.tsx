f8250b323dc3400683d94b717953edd25b271773
Raman Krytski <rkeydevelop@gmail.com>
2024-02-05 10:09:41 +0100

    fix: multiple type of answer means one of four answers instead of multiple answers

--- src/shared/QuestionWindow/Answers/Answers.tsx
+++ src/shared/QuestionWindow/Answers/Answers.tsx
@@ -11,25 +11,25 @@ export const Answers: FC<IProps> = ({type}) => {
         return (
             <div className={styles.Checkbox}>
                 <div className={styles.answer}>
-                    <input className={styles.hiddenInput} type="checkbox" name="checkbox" id="1" />
+                    <input className={styles.hiddenInput} type="radio" name="checkbox" id="1" />
                     <label className={styles.checkboxLabel} title='Answer 1' htmlFor='1'>
                         1. answer 1
                     </label>
                 </div>
                 <div className={styles.answer}>
-                    <input className={styles.hiddenInput} type="checkbox" name="checkbox" id="2" />
+                    <input className={styles.hiddenInput} type="radio" name="checkbox" id="2" />
                     <label className={styles.checkboxLabel} title='Answer 2' htmlFor='2'>
                         2. answer 2
                     </label>
                 </div>
                 <div className={styles.answer}>
-                    <input className={styles.hiddenInput} type="checkbox" name="checkbox" id="3" />
+                    <input className={styles.hiddenInput} type="radio" name="checkbox" id="3" />
                     <label className={styles.checkboxLabel} title='Answer 3' htmlFor='3'>
                         3. answer 3
                     </label>
                 </div>
                 <div className={styles.answer}>
-                    <input className={styles.hiddenInput} type="checkbox" name="checkbox" id="4" />
+                    <input className={styles.hiddenInput} type="radio" name="checkbox" id="4" />
                     <label className={styles.checkboxLabel} title='Answer 4' htmlFor='4'>
                         4. answer 4
                     </label>
