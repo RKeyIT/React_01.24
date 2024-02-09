b097ab0e61d72f87434f9503d9335da4e078cb36
Raman Krytski <rkeydevelop@gmail.com>
2024-02-09 10:23:12 +0100

    feat: close button have navigation logic now --> next step is portal

--- src/entities/CloseButton/CloseButton.tsx
+++ src/entities/CloseButton/CloseButton.tsx
@@ -1,21 +1,40 @@
-import { Link } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import styles from './CloseButton.module.css'
 import { FC } from 'react'
 import { URLs } from '../../router/router.types'
 
 interface IProps {
   title?: string
-  callback?: () => void
 }
 
-export const CloseButton: FC<IProps> = ({ title = 'close', callback }) => {
+/* TODO
+- Upon pressing the “End quiz” button on the main quiz screen we should see a 
+  modal window with some text and 2 buttons - “Cancel” and “Confirm”. 
+  Upon pressing “Cancel” button, we just close this modal window, but upon pressing 
+  “Confirm” button we need to navigate user back to configuration screen
+
+- After answering the last question or if timer ends on the main quiz screen, 
+  we need to navigate users to the result screen
+*/
+
+export const CloseButton: FC<IProps> = ({ title = 'Go home' }) => {
+  const path = useLocation().pathname
+  const navigate = useNavigate()
+
+  const disabled = path === URLs.HOME ? true : false
+
+  const onCloseButton = () => {
+    if(path === URLs.HOME) return
+
+    if (path !== URLs.GAME) {
+      return navigate(URLs.HOME)
+    }
+  }
+  
   return (
-    <div className={styles.CloseButtonContainer}>
-      <Link to={URLs.HOME}>
-        <button onClick={callback} type="button" title={title} className={styles.CloseButton}>
-          x
-        </button>
-      </Link>
-    </div>
+      <button disabled={disabled} onClick={onCloseButton} 
+              type="button" title={title} className={styles.CloseButton}>
+        x
+      </button>
   )
 }
