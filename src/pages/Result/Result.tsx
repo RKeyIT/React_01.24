9d499154c6a7d265e3b052b30d54d451d7f2b4db
Raman Krytski <rkeydevelop@gmail.com>
2024-02-13 20:34:53 +0100

    feat: added dynamic data to Result page

--- src/pages/Result/Result.tsx
+++ src/pages/Result/Result.tsx
@@ -8,8 +8,16 @@ import styles from './Result.module.css'
 import { FC } from 'react'
 import { URLS } from '../../router/router.types'
 import { PageNames } from '../../global.types'
+import { useAppSelector } from '../../store'
 
 export const Result: FC = () => {
+  const answers = useAppSelector(store => store.game.player_answers)
+
+  let rightAnswers = 0;
+  answers.forEach((el) => {
+    if (el) rightAnswers++
+  })
+
   return (
     <div className={styles.Result}>
       <Heading pageName={PageNames.RESULT} />
@@ -18,7 +26,7 @@ export const Result: FC = () => {
         <Table name="Quiz configuration" />
       </div>
       <div className={styles.summary}>
-        <TextField>You answered 5 out of 10 questions correctly</TextField>
+        <TextField>You answered {rightAnswers} out of {answers.length} questions correctly</TextField>
         <TextField>Time spent: 05:29</TextField>
         <ProgressBar />
       </div>
--- src/shared/Table/Table.tsx
+++ src/shared/Table/Table.tsx
@@ -1,3 +1,4 @@
+import { useAppSelector } from '../../store'
 import styles from './Table.module.css'
 import { FC } from 'react'
 
@@ -9,24 +10,26 @@ interface IProps {
 }
 
 export const Table: FC<IProps> = ({ name = 'Table' }) => {
+  const {category, difficulty, type, time} = useAppSelector(store => store.config)
+
   return (
     <div className={styles.Table}>
       <div className={styles.tableName}>{name}:</div>
       <div className={styles.row}>
-        <div className={styles.category}>type</div>
-        <div className={styles.description}>multiple</div>
-      </div>
-      <div className={styles.row}>
         <div className={styles.category}>category</div>
-        <div className={styles.description}>Art</div>
-      </div>
-      <div className={styles.row}>
-        <div className={styles.category}>time</div>
-        <div className={styles.description}>one minute</div>
+        <div className={styles.description}>{category}</div>
       </div>
       <div className={styles.row}>
         <div className={styles.category}>difficulty</div>
-        <div className={styles.description}>hard</div>
+        <div className={styles.description}>{difficulty}</div>
+      </div>
+      <div className={styles.row}>
+        <div className={styles.category}>type</div>
+        <div className={styles.description}>{type}</div>
+      </div>
+      <div className={styles.row}>
+        <div className={styles.category}>time</div>
+        <div className={styles.description}>{time} {time === '1' ? 'minute' : 'minutes'}</div>
       </div>
     </div>
   )
