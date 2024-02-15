2149856c0144ba0fe4b0ba554f71c81a30c49fb7
Raman Krytski <rkeydevelop@gmail.com>
2024-02-15 12:46:57 +0100

    refactoring: Added button disable logic related on current route and Game and Result buttons were removed

--- src/features/Navigation/Navigation.tsx
+++ src/features/Navigation/Navigation.tsx
@@ -1,18 +1,30 @@
 import { NavButton } from '../../shared/NavButton/NavButton'
 import styles from './Navigation.module.css'
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { URLS } from '../../router/router.types'
 import { PageNames } from '../../global.types'
+import { useLocation } from 'react-router-dom'
 
 export const Navigation: FC = () => {
-  // NOTE - Are we need the Game page in navigation?
+  const path = useLocation().pathname
+
+  const [isHomeDisabled, setHomeDisabled] = useState(false)
+  const [isStatisticsDisabled, setStatisticsDisabled] = useState(false)
+
+  useEffect(() => {
+    if (path === URLS.GAME) {
+      setHomeDisabled(true)
+      setStatisticsDisabled(true)
+    } else {
+      setHomeDisabled(false)
+      setStatisticsDisabled(false)
+    }
+  }, [path])
 
   return (
     <nav className={styles.Navigation}>
-      <NavButton navLink={URLS.HOME} content={PageNames.HOME} />
-      <NavButton navLink={URLS.GAME} content={PageNames.GAME} />
-      <NavButton navLink={URLS.RESULT} content={PageNames.RESULT} />
-      <NavButton navLink={URLS.STATISTICS} content={PageNames.STATISTICS} />
+      <NavButton disabled={isHomeDisabled} navLink={URLS.HOME} content={PageNames.HOME} />
+      <NavButton disabled={isStatisticsDisabled} navLink={URLS.STATISTICS} content={PageNames.STATISTICS} />
     </nav>
   )
 }
--- src/shared/NavButton/NavButton.tsx
+++ src/shared/NavButton/NavButton.tsx
@@ -1,21 +1,22 @@
 import { NavLink } from 'react-router-dom'
-import { PageNames } from '../../context/GameContext/GameContext.types'
 import { URLS } from '../../router/router.types'
 import styles from './NavButton.module.css'
 import { FC } from 'react'
+import { PageNames } from '../../global.types'
 
 interface IProps {
   content: PageNames
   navLink: URLS
+  disabled?: boolean
 }
 
-export const NavButton: FC<IProps> = ({ navLink, content }) => {
+export const NavButton: FC<IProps> = ({ navLink, content, disabled }) => {
   const navLinkClass = `${styles.a}`
   const activeNav = `${styles.a} ${styles.active}`
 
   return (
     <NavLink to={navLink} className={({ isActive }) => (isActive ? activeNav : navLinkClass)}>
-      <button type="button" className={styles.NavButton}>
+      <button disabled={disabled} type="button" className={styles.NavButton}>
         {content}
       </button>
     </NavLink>
