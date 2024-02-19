a48ee3d56c32db5761038de72dfbf26b1b8489b6
Raman Krytski <rkeydevelop@gmail.com>
2024-02-19 12:54:11 +0100

    feat: created Statistics component markup

--- src/pages/Statistics/Statistics.tsx
+++ src/pages/Statistics/Statistics.tsx
@@ -1,14 +1,50 @@
 import { useNavigate } from 'react-router-dom'
-import { PageNames } from '../../global.types'
+import { ITableRow, PageNames } from '../../global.types'
 import { Heading } from '../../shared/Heading/Heading'
 import styles from './Statistics.module.css'
 import { FC } from 'react'
 import { URLS } from '../../router/router.types'
 import { Button } from '../../shared/Button/Button'
 import { Table } from '../../shared/Table/Table'
+import { useAppSelector } from '../../store'
 
 export const Statistics: FC = () => {
   const navigate = useNavigate()
+  const { 
+    OverallQuestionCount,
+    OverallTimeSpent,
+    CorrectAnswerCount,
+    CorrectAnswerPercentage,
+    CategoriesCount,
+    DifficultiesCount,
+    TypeCount
+  } = useAppSelector(state => state.persistor)
+  
+  const OveralTableRows: ITableRow[] = [
+    { category: 'Overal count of questions', description: OverallQuestionCount },
+    { category: 'Count of correct answers', description: CorrectAnswerCount },
+    { category: 'Correct answers percentage', description: CorrectAnswerPercentage },
+    { category: 'Overal time in quiz app', description: OverallTimeSpent },
+  ]
+
+  const DifficultyTableRows: ITableRow[] = [
+    { category: 'easy', description: DifficultiesCount.easy },
+    { category: 'medium', description: DifficultiesCount.medium },
+    { category: 'hard', description: DifficultiesCount.hard },
+  ]
+
+  const TypeTableRows: ITableRow[] = [
+    { category: 'boolean', description: TypeCount.boolean },
+    { category: 'multiple', description: TypeCount.multiple },
+  ]
+
+  const CategoryTableRows: ITableRow[] = []
+
+  for (const key in CategoriesCount) {
+    CategoryTableRows.push({ category: key, description: CategoriesCount[key] })
+  }
+
+  CategoryTableRows.sort((a, b) => +a.description - +b.description)
   
   const toHome = () => {
     navigate(URLS.HOME)
@@ -17,7 +53,12 @@ export const Statistics: FC = () => {
   return (
     <div className={styles.Statistics}>
       <Heading pageName={PageNames.STATISTICS} />
-      <Table name='My general statistic' rows={[]} />
+      <div className={styles.tables}>
+        <Table name='Overal stats' rows={OveralTableRows} />
+        <Table name='Difficulties' rows={DifficultyTableRows} />
+        <Table name='Types' rows={TypeTableRows} />
+        <Table name='Categories' rows={CategoryTableRows} />
+      </div>
       <Button callback={toHome} content='To home page'/>
     </div>
   )
