7edf98b4453112aa692bf7123132df9be7e3636a
Raman Krytski <rkeydevelop@gmail.com>
2024-02-19 12:04:14 +0100

    feat: created persistor with its own slice and whitelist for it

---
+++ src/store/persistorSlice.tsx
@@ -0,0 +1,57 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchGameData } from "./gameSlice";
+
+interface ICategoriesCount {
+    [key: string]: number
+}
+
+interface IDifficultiesCount {
+    easy: number
+    medium: number
+    hard: number
+}
+
+interface ITypeCount {
+    boolean: number,
+    multiple: number
+}
+
+interface IPersistorState {
+    OverallQuestionCount: number
+    OverallTimeSpent: number
+    CorrectAnswerCount: number
+    CorrectAnswerPercentage: number
+    CategoriesCount: ICategoriesCount
+    DifficultiesCount: IDifficultiesCount
+    TypeCount: ITypeCount
+}
+
+const initialState: IPersistorState = {
+    OverallQuestionCount: 0,
+    OverallTimeSpent: 0,
+    CorrectAnswerCount: 0,
+    CorrectAnswerPercentage: 0,
+    CategoriesCount: {},
+    DifficultiesCount: {
+        easy: 0,
+        medium: 0,
+        hard: 0,
+    },
+    TypeCount: {
+        boolean: 0,
+        multiple: 0,
+    }
+}
+
+const persistorSlice = createSlice({
+    name: 'persistor',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchGameData.pending, (state) => {
+            state.CategoriesCount['SOME FIELD'] = 0
+        })
+    },
+})
+
+export const persistorReducer = persistorSlice.reducer
