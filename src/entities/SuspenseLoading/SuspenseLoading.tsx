71093789f6872767e22a662709343c48256f795a
Raman Krytski <rkeydevelop@gmail.com>
2024-02-28 09:27:27 +0100

    feat: animated loader and modal window

--- src/entities/SuspenseLoading/SuspenseLoading.tsx
+++ src/entities/SuspenseLoading/SuspenseLoading.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'framer-motion'
 import styles from './SuspenseLoading.module.css'
 import { FC } from 'react'
 import { createPortal } from 'react-dom'
@@ -6,9 +7,14 @@ export const SuspenseLoading: FC = () => {
   const portalTarget = document.getElementById('ContentContainer')
 
   const markup = (
-    <div className={styles.Loading}>
-      <div className={styles.circle}></div>
-    </div>
+    <AnimatePresence>
+      <motion.div className={styles.Loading}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1}}
+        exit={{ opacity: 0 }}>
+        <div className={styles.circle}></div>
+      </motion.div>
+    </AnimatePresence>
   )
 
   return <>{portalTarget && createPortal(markup, portalTarget)}</>
